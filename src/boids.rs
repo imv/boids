26c5cf2e24f00584f532994d65a82ad9848d0f5b
Mikhail Ivko <ivko.mv@gmail.com>
2014-07-14 02:26:54 +0700

    Dummy piston game.

--- src/boids.rs
+++ src/boids.rs
@@ -1,3 +1,26 @@
+extern crate piston;
+extern crate sdl2_game_window;
+
+use piston::{Game, GameWindowSettings, GameIteratorSettings};
+use sdl2_game_window::GameWindowSDL2;
+
+pub struct App;
+
+impl Game for App {
+}
+
 fn main() {
-    println!("Hello world!");
+    let mut window = GameWindowSDL2::new(
+        GameWindowSettings {
+            title: "Boids".to_string(),
+            ..GameWindowSettings::default()
+        }
+    );
+    let mut app = App;
+    app.run(&mut window,
+        &GameIteratorSettings {
+            updates_per_second: 120,
+            max_frames_per_second: 60
+        }
+    );
 }
