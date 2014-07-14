5447df2af32d4c3fac424240c5a6a1a2c89694e7
Mikhail Ivko <ivko.mv@gmail.com>
2014-07-15 03:45:17 +0700

    Draw sky.

--- src/boids.rs
+++ src/boids.rs
@@ -1,12 +1,32 @@
 extern crate piston;
+extern crate graphics;
 extern crate sdl2_game_window;
+extern crate opengl_graphics;
 
-use piston::{Game, GameWindowSettings, GameIteratorSettings};
+use piston::{Game, GameWindowSettings, GameIteratorSettings, RenderArgs};
+use graphics::{Context, AddColor, Draw};
 use sdl2_game_window::GameWindowSDL2;
+use opengl_graphics::Gl;
 
-pub struct App;
+pub struct App {
+    gl: Gl
+}
+
+impl App {
+    fn new() -> App {
+        App {
+            gl: Gl::new()
+        }
+    }
+}
 
 impl Game for App {
+    fn render(&mut self, args: &RenderArgs) {
+        self.gl.viewport(0, 0, args.width as i32, args.height as i32);
+        Context::abs(args.width as f64, args.height as f64)
+            .rgb(0.25, 0.5, 1.0)
+            .draw(&mut self.gl);
+    }
 }
 
 fn main() {
@@ -16,7 +36,7 @@ fn main() {
             ..GameWindowSettings::default()
         }
     );
-    let mut app = App;
+    let mut app = App::new();
     app.run(&mut window,
         &GameIteratorSettings {
             updates_per_second: 120,
