26fa3decb119aec3fd2cfc1465b83ac69a95c024
Mikhail Ivko <ivko.mv@gmail.com>
2014-09-03 03:43:12 +0700

    Initialize boids.

--- src/boids.rs
+++ src/boids.rs
@@ -1,21 +1,39 @@
+extern crate rand;
 extern crate piston;
 extern crate graphics;
 extern crate sdl2_game_window;
 extern crate opengl_graphics;
+extern crate cgmath;
 
+use rand::{Rand, XorShiftRng};
 use piston::{Game, GameWindowSettings, GameIteratorSettings, RenderArgs};
 use graphics::{Context, AddColor, Draw};
 use sdl2_game_window::GameWindowSDL2;
 use opengl_graphics::Gl;
+use cgmath::point::Point2;
+use cgmath::vector::Vector2;
+
+pub struct Boid {
+    pos: Point2<f64>,
+    vel: Vector2<f64>,
+}
+
+pub static BOID_COUNT: uint = 50;
 
 pub struct App {
-    gl: Gl
+    gl: Gl,
+    boids: Vec<Boid>,
 }
 
 impl App {
     fn new() -> App {
+        let mut rng = XorShiftRng::new_unseeded();
         App {
-            gl: Gl::new()
+            gl: Gl::new(),
+            boids: Vec::from_fn(BOID_COUNT, |_| Boid {
+                pos: Point2 { x: Rand::rand(&mut rng), y: Rand::rand(&mut rng) },
+                vel: Vector2 { x: Rand::rand(&mut rng), y: Rand::rand(&mut rng) },
+            })
         }
     }
 }
@@ -23,7 +41,8 @@ impl App {
 impl Game for App {
     fn render(&mut self, args: &RenderArgs) {
         self.gl.viewport(0, 0, args.width as i32, args.height as i32);
-        Context::abs(args.width as f64, args.height as f64)
+        let context = Context::abs(args.width as f64, args.height as f64);
+        context
             .rgb(0.25, 0.5, 1.0)
             .draw(&mut self.gl);
     }
