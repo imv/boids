e62ee0da613e1ac4cec8ec945a6e1a7db22c5b42
Mikhail Ivko <ivko.mv@gmail.com>
2014-09-06 17:34:13 +0700

    Implement boids behavior.

--- src/boids.rs
+++ src/boids.rs
@@ -6,6 +6,7 @@ extern crate opengl_graphics;
 extern crate cgmath;
 extern crate shader_version;
 
+use std::num::{abs, Zero};
 use rand::{Rand, XorShiftRng};
 use rand::distributions::{Normal, IndependentSample};
 use piston::{
@@ -22,8 +23,9 @@ use graphics::{
 use sdl2_game_window::WindowSDL2;
 use opengl_graphics::Gl;
 use cgmath::{
+    ApproxEq,
     Point, Point2,
-    Vector, Vector2
+    Vector, EuclideanVector, Vector2
 };
 use shader_version::opengl::OpenGL_3_2;
 
@@ -34,6 +36,11 @@ pub struct Boid {
 
 pub static BOID_COUNT: uint = 50;
 pub static BOID_RADIUS: f64 = 0.01;
+pub static VISION_ANGLE: f64 = 2.0;
+pub static CROWD_RADIUS: f64 = 0.1;
+pub static FLOCK_RADIUS: f64 = 0.2;
+pub static ACCEL_FACTOR: f64 = 10.0;
+pub static ALIGN_FACTOR: f64 = 0.5;
 
 pub struct App {
     boids: Vec<Boid>,
@@ -67,9 +74,35 @@ impl App {
         }
     }
 
-    fn update(&mut self, dt: f64) {
+    fn update_pos(&mut self, dt: f64) {
         for b in self.boids.mut_iter() {
             b.pos.add_self_v(&b.vel.mul_s(dt))
+        }
+    }
+
+    fn update_vel(&mut self, dt: f64) {
+        let accels: Vec<Vector2<f64>> = self.boids.iter().map(|b1|
+            self.boids.iter()
+                .filter(|b2|
+                    !b2.pos.approx_eq(&b1.pos)
+                ).filter(|b2| {
+                    let dist_v = b2.pos.sub_p(&b1.pos);
+                    // within vision angle
+                    abs(b1.vel.angle(&dist_v).s) <= VISION_ANGLE &&
+                    // within flocking distance
+                    dist_v.length2() <= FLOCK_RADIUS*FLOCK_RADIUS
+                }).fold(Zero::zero(), |accel: Vector2<f64>, b2| {
+                    let dist_v = b2.pos.sub_p(&b1.pos);
+                    accel
+                        // cohesion & separation
+                        + (dist_v - dist_v.normalize_to(CROWD_RADIUS))
+                            .mul_s(ACCEL_FACTOR)
+                        // alignment
+                        + (b2.vel - b1.vel).mul_s(ALIGN_FACTOR)
+                })
+        ).collect();
+        for (ref mut b, ref accel) in self.boids.mut_iter().zip(accels.iter()) {
+            b.vel.add_self_v(&accel.mul_s(dt))
         }
     }
 }
@@ -95,8 +128,10 @@ fn main() {
                 gl.viewport(0, 0, args.width as i32, args.height as i32);
                 app.render(&mut gl);
             },
-            Update(ref args) =>
-                app.update(args.dt),
+            Update(ref args) => {
+                app.update_pos(args.dt);
+                app.update_vel(args.dt);
+            },
             _ => {},
         }
     }
