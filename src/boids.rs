e25c31fc1ff0ae8f88152017831643a7edd1c959
Mikhail Ivko <ivko.mv@gmail.com>
2015-05-28 11:11:25 +0600

    Replace cgmath with nalgebra

--- src/boids.rs
+++ src/boids.rs
@@ -4,7 +4,7 @@ extern crate piston;
 extern crate graphics;
 extern crate sdl2_window;
 extern crate opengl_graphics;
-extern crate cgmath;
+extern crate nalgebra as na;
 
 use num::{abs, Zero};
 use rand::{Rand, XorShiftRng};
@@ -14,15 +14,14 @@ use piston::event::{Events, RenderEvent, UpdateEvent};
 use graphics::{Context, Transformed};
 use sdl2_window::Sdl2Window;
 use opengl_graphics::{GlGraphics, OpenGL};
-use cgmath::{
-    ApproxEq,
-    Point, Point2,
-    Vector, EuclideanVector, Vector2
+use na::{
+    ApproxEq, RotationTo, Norm, Axpy,
+    Pnt2, Vec2
 };
 
 pub struct Boid {
-    pos: Point2<f64>,
-    vel: Vector2<f64>,
+    pos: Pnt2<f64>,
+    vel: Vec2<f64>,
 }
 
 pub static BOID_COUNT: u32 = 50;
@@ -45,8 +44,8 @@ impl App {
         let normal = Normal::new(0.0, ALONE_VEL*0.5f64.sqrt());
         App {
             boids: (0..BOID_COUNT).map(|_| Boid {
-                pos: Point2 { x: Rand::rand(&mut rng), y: Rand::rand(&mut rng) },
-                vel: Vector2 {
+                pos: Pnt2 { x: Rand::rand(&mut rng), y: Rand::rand(&mut rng) },
+                vel: Vec2 {
                     x: normal.ind_sample(&mut rng),
                     y: normal.ind_sample(&mut rng)
                 }
@@ -66,7 +65,7 @@ impl App {
 
     fn update_pos(&mut self, dt: f64) {
         for b in self.boids.iter_mut() {
-            b.pos.add_self_v(&b.vel.mul_s(dt))
+            b.pos.axpy(&dt, b.vel.as_pnt())
         }
     }
 
@@ -80,35 +79,33 @@ impl App {
                 0.0
             }
         }
-        let accels: Vec<Vector2<f64>> = self.boids.iter().map(|b1| {
+        let accels: Vec<Vec2<f64>> = self.boids.iter().map(|b1| {
             let flocking = self.boids.iter()
                 .filter(|b2|
                     !b2.pos.approx_eq(&b1.pos)
                 ).filter(|b2| {
-                    let dist_v = b2.pos.sub_p(&b1.pos);
+                    let dist_v = b2.pos - b1.pos;
                     // within vision angle
-                    abs(b1.vel.angle(&dist_v).s) <= VISION_ANGLE &&
+                    abs(b1.vel.angle_to(&dist_v)) <= VISION_ANGLE &&
                     // within flocking distance
-                    dist_v.length2() <= FLOCK_RADIUS*FLOCK_RADIUS
-                }).fold(Zero::zero(), |accel: Vector2<f64>, b2| {
-                    let dist_v = b2.pos.sub_p(&b1.pos);
+                    dist_v.sqnorm() <= FLOCK_RADIUS*FLOCK_RADIUS
+                }).fold(Zero::zero(), |accel: Vec2<f64>, b2| {
+                    let dist_v = b2.pos - b1.pos;
                     accel
                         // cohesion & separation
-                        + (dist_v - dist_v.normalize_to(CROWD_RADIUS))
-                            .mul_s(ACCEL_FACTOR)
+                        + (dist_v - dist_v*(CROWD_RADIUS/dist_v.norm()))*ACCEL_FACTOR
                         // alignment
-                        + (b2.vel - b1.vel).mul_s(ALIGN_FACTOR)
+                        + (b2.vel - b1.vel)*ALIGN_FACTOR
                 });
-            let keep_vel = (b1.vel.normalize_to(ALONE_VEL) - b1.vel)
-                .mul_s(KEEP_VEL_FACTOR);
-            let steer_to_screen = Vector2 {
+            let keep_vel = (b1.vel*(ALONE_VEL/b1.vel.norm()) - b1.vel)*KEEP_VEL_FACTOR;
+            let steer_to_screen = Vec2 {
                 x: towards_0_1(b1.pos.x),
                 y: towards_0_1(b1.pos.y)
             };
             flocking + keep_vel + steer_to_screen
         }).collect();
         for (ref mut b, ref accel) in self.boids.iter_mut().zip(accels.iter()) {
-            b.vel.add_self_v(&accel.mul_s(dt))
+            b.vel.axpy(&dt, accel)
         }
     }
 }
